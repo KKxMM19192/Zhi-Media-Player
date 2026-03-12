b20bd002cf0e947608293ce54e2c93065847f827
Komputer <3070581596@qq.com>
2026-03-13 02:43:17 +0800

    feat(zmp_fe): 前端项目构建

---
+++ zmp-fe/src/renderer/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container class="main_page">
+    <Sidebar/>
+
+    <el-container direction="vertical">
+      <el-header height="20px">
+        <div class="main_title">Title</div>
+      </el-header>
+
+      <el-main>
+        <el-scrollbar>
+          <div>
+            <h2>Content</h2>
+          </div>
+        </el-scrollbar>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+  import Sidebar from './components/Sidebar.vue'
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  .main_page {
+    height: 100vh;
+    width: 100vw;
+  }
+  .main_title {
+    border-bottom: 1px solid black;
+    display: flex;
+    align-items: center;
+  }
+</style>
---
+++ zmp-fe/src/renderer/src/components/Sidebar.vue
@@ -0,0 +1,91 @@
+<template>
+    <el-aside width="210px" class="sidebar_container">
+        <div class="sidebar_title">
+            <el-icon :size="25" color="#409EFF"><Headset/></el-icon>
+            <span class="sidebar_title_text">Zhi Media Player</span>
+        </div>
+
+        <el-menu
+            default-active="1"
+            class="sidebar_body"
+            :border="false">
+            <el-menu-item-group title="搜索">
+                <el-menu-item>
+                    <div>Search Bar(Unfinished)</div>
+                </el-menu-item>
+            </el-menu-item-group>
+
+            <el-menu-item-group title="推荐">
+                <el-menu-item index="1">
+                    <el-icon><Compass/></el-icon>
+                    <span>发现音乐</span>
+                </el-menu-item>
+
+                <el-menu-item index="2">
+                    <el-icon><Mic /></el-icon>
+                    <span>播客</span>
+                </el-menu-item>
+            </el-menu-item-group>
+
+            <el-menu-item-group title="我的音乐">
+                <el-menu-item index="3">
+                    <el-icon><FolderOpened /></el-icon>
+                    <span>本地音乐</span>
+                </el-menu-item>
+
+                <el-menu-item index="4">
+                    <el-icon><Star /></el-icon>
+                    <span>我的收藏</span>
+                </el-menu-item>
+
+                <el-menu-item index="5">
+                    <el-icon><Download /></el-icon>
+                    <span>下载管理</span>
+                </el-menu-item>
+            </el-menu-item-group>
+        </el-menu>
+    </el-aside>
+</template>
+
+<style scoped>
+    .sidebar_container {
+        background-color: rgba(255, 255, 255, 1);
+        display: flex;
+        flex-direction: column;
+        border-right: 1px solid #e1e4e8;
+        height: 100vh;
+    }
+
+    .sidebar_title {
+        padding: 20px 10px 10px 25px;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        -webkit-app-region: drag;
+    }
+
+    .sidebar_title_text {
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .sidebar_body {
+        background-color: transparent;
+    }
+
+    :deep(.el-menu-item) {
+        height: 45px;
+        line-height: 45px;
+        margin: 4px 12px;
+        border-radius: 8px;
+    }
+
+    :deep(.el-menu-item.is-active) {
+        background-color: #409EFF;
+        color: #fff;
+    }
+
+    :deep(.el-menu-item:hover) {
+        background-color: skyblue;
+    }
+</style>
